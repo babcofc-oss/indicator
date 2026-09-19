26a352e192b50f4eb4fe1d9f64f13cbbd58b8bce
babcofc-oss <babcofc@gmail.com>
2026-09-19 17:51:11 -0400

    Add portraits for player cards

---
+++ lib/player-images.ts
@@ -0,0 +1,31 @@
+// Sleeper player directory IDs, matched to the roster on 2026-09-19.
+// Portraits are served by Sleeper; these are player photos, not game-action images.
+const sleeperIds: Record<string, string> = {
+  'caleb-douglas': '13296',
+  'terry-mclaurin': '5927',
+  'jahmyr-gibbs': '9221',
+  'nico-collins': '7569',
+  'braelon-allen': '11576',
+  'jayden-reed': '10222',
+  'mike-washington': '13305',
+  'jordan-mason': '8408',
+  'cade-otton': '8111',
+  'justin-herbert': '6797',
+  'ja-marr-chase': '7564',
+  'jaxon-smith-njigba': '9488',
+  'jaylen-wright': '11643',
+  'cole-kmet': '6826',
+  'rj-harvey': '12489',
+  'ceedee-lamb': '6786',
+  'trey-benson': '11589',
+  'brian-thomas': '11631',
+  'saquon-barkley': '4866',
+  'michael-penix': '11559',
+  'khalil-shakir': '8134',
+  'kendre-miller': '9757',
+}
+
+export function playerPortrait(id: string): string | undefined {
+  const sleeperId = sleeperIds[id]
+  return sleeperId && `https://sleepercdn.com/content/nfl/players/${sleeperId}.jpg`
+}
